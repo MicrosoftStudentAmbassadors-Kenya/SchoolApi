45c66e3db5b16ba5b2f1306b4264fafacd7a4d66
edwin muraya <edwinkamaumuraya0@outlook.com>
2019-11-02 20:32:32 +0300

    refactored code using the clean architecture

---
+++ SchoolProjects/Domain/Unit.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Unit
+    {
+        [Key]
+        public int UnitId { get; set; }
+
+        [Required]
+        public string UnitName { get; set; }
+        [StringLength(100,MinimumLength = 10,ErrorMessage = "A unit must have some descriptions")]
+        public string Description { get; set; }
+        public bool? HasLab { get; set; }
+
+        [MaxLength(100,ErrorMessage = "UnitMark must not be more than 100marks")]
+        public double UnitMark { get; set; }
+        public Student Student { get; set; }
+    }
+}
